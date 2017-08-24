e3f3f74879e35806fc36e5d1c8b171e531450e2f
Erick Winston <ewinston@us.ibm.com>
2017-08-24 03:08:44 -0400

    minor commit

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 import os
 for key, value in os.environ.items():
     print('{0} = "{1}"'.format(key, value))
+
+TRAVIS_FORK_PULL_REQUEST = False
+if ('TRAVIS_PULL_REQUEST_SLUG' in os.environ
+    and not os.environ['TRAVIS_PULL_REQUEST_SLUG']):
+    if os.environ['TRAVIS_REPO_SLUG'] != os.environ['TRAVIS_PULL_REQUEST_SLUG']:
+        TRAVIS_FORK_PULL_REQUEST = True
+print('TRAVIS_FORK_PULL_REQUEST', TRAVIS_FORK_PULL_REQUEST)
